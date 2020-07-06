ec253b0f37cab33499ecfd8760f1f3650c82bb83
Lucky728 <61776822+Lucky728@users.noreply.github.com>
2020-07-06 18:18:24 +0700

    Add files via upload

---
+++ produk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        public string kodeProduk { get; set; }
+        public string namaProduk { get; set; }
+        public double hargaBeli { get; set; }
+        public double hargaJual { get; set; }
+
+    }
+}
